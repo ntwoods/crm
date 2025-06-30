04bb77a00149e620ce99a676e4e0438da2169dba
ntwoods <mis01@ntwoods.com>
2025-06-30 14:14:47 +0530

    Update reload.js

--- js/reload.js
+++ js/reload.js
@@ -1,9 +1,14 @@
-    document.addEventListener("visibilitychange", function () {
-      if (document.visibilityState === "visible") {
-        location.reload();
-      }
-    });
+// reload.js
+let isModalOpen = false; // New global flag
 
-    setInterval(() => {
-      location.reload();
-    }, 10000); // Reload every 10 seconds
+document.addEventListener("visibilitychange", function () {
+  if (document.visibilityState === "visible" && !isModalOpen) { // Check the flag
+    location.reload();
+  }
+});
+
+setInterval(() => {
+  if (!isModalOpen) { // Check the flag before reloading
+    location.reload();
+  }
+}, 10000); // Reload every 10 seconds
