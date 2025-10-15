40546327e174f719aaf2467ed5fe645e24040e3a
NT Woods (P) Ltd <mis01@ntwoods.com>
2025-10-15 13:08:56 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -43,3 +43,5 @@ async function fetchAndUpdateCount(levelKey, elementId, cardId) {
         fetchAndUpdateCount('level7', 'level7-count', 'level7-card');
         fetchAndUpdateCount('level8', 'level8-count', 'level8-card');
         fetchAndUpdateCount('level9', 'level9-count', 'level9-card');        
+        fetchAndUpdateCount('level10', 'level10-count', 'level10-card');        
+        fetchAndUpdateCount('level11', 'level11-count', 'level11-card');        
